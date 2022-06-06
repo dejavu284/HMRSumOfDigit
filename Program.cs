2129e61c75a1a2ac6be8cea268525a819ab06f6a
dejavu284 <skuratov.1@mail.ru>
2022-06-06 22:15:21 +0700

    Сумма цифр числа, через РЕКУРСИЮ.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HMRSumOfDigit
+{
+    class Program
+    {
+        static int SumOf(int value)
+        {
+            if (value < 10)
+                return value;
+            return value % 10 + SumOf(value /= 10);
+        }
+        static void Main(string[] args)
+        {
+            int sum = SumOf(561199);
+            Console.WriteLine(sum);
+            float max = float.MaxValue;
+            double Max = double.MaxValue;
+            float s = (float)Max;
+            Console.WriteLine(s);
+            Console.WriteLine(max);
+        }
+    }
+}
